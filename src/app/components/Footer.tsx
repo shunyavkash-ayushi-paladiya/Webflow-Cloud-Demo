ae79de2a9227108944d17fee5a12a9d67c984db9
deep bhimani <deep.bhimani@shunyavkash.com>
2025-10-27 12:55:38 +0530

    code formated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-      <Block tag="footer">
-          <Block className="container mx-auto">
-              <Link href="../">Main Home</Link>
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-              <Link href="/contact">Contact</Link>
-          </Block>
+    <Block tag="footer">
+        <Block className="container mx-auto">
+            <Link href="./">Main Home</Link>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+        </Block>
     </Block>
   )
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-      <Block tag="header">
-          <Block className="container mx-auto">
-              <Link href="../">Main Home</Link>
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-              <Link href="/contact">Contact</Link>
-          </Block>
-      </Block>
+    <Block tag="header">
+        <Block className="container mx-auto">
+            <Link href="./">Main Home</Link>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+        </Block>
+    </Block>
   )
 }
