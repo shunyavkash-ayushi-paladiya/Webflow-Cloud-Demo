345b980e2caf9206fcca18aff60e810f9be4c026
deep bhimani <deep.bhimani@shunyavkash.com>
2025-10-27 12:45:15 +0530

    change navbar home page link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
       <Block tag="header">
           <Block className="container mx-auto">
-              <Link href="/">Home</Link>
+              <Link href="../">Home</Link>
               <Link href="/about">About</Link>
               <Link href="/contact">Contact</Link>
           </Block>
