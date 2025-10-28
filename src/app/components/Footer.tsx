2aaec9707e0e3b87e154319983c53abdad462167
deep bhimani <deep.bhimani@shunyavkash.com>
2025-10-28 10:02:04 +0530

    updated links list in navbar & footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
   return (
     <Block tag="footer">
         <Block className="container mx-auto">
-            <Link href="./">Main Home</Link>
+            {/* <Link href="./">Main Home</Link> */}
+            {/* <Link href="./investment-strategies">Investment Strategies</Link> */}
             <Link href="/">Home</Link>
-            <Link href="./investment-strategies">Investment Strategies</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
         </Block>
