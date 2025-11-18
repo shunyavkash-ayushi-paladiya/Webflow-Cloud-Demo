bdd3b9299d92ab011eb7082f165de9f7ab350c3b
deep bhimani <deep.bhimani@shunyavkash.com>
2025-11-18 12:15:42 +0530

    init

--- src/app/contact/ContactClient.tsx
+++ src/app/contact/ContactClient.tsx
@@ -1,39 +1,25 @@
-'use client';
+"use client";
 
-import { Section, Block } from '@/devlink/_Builtin';
-import Link from 'next/link';
-import TextFiled from '../components/Textfield';
+import { Section, Block } from "@/devlink/_Builtin";
+import Link from "next/link";
+import TextFiled from "../components/Textfield";
 
 export default function Contact() {
   return (
     <Section tag="section" className="min-h-[50vh] flex items-center justify-center">
       <Block tag="div" className="container">
         <Block tag="div" className="hero-split text-center max-w-150 mx-auto">
-          <h1 className="mb-6 text-4xl font-bold bg-gradient-to-l from-[#3245ff] to-[#bc52ee] bg-clip-text text-transparent">
-            Contact
-          </h1>
+          <h1 className="mb-6 text-4xl font-bold bg-gradient-to-l from-[#3245ff] to-[#bc52ee] bg-clip-text text-transparent">Contact</h1>
 
           <Block className="block mb-5">
             <img src="window.svg" className="w-50" alt="Picture of the author" />
           </Block>
 
-          <TextFiled
-            id="input"
-            name="input"
-            placeholder="write in input"
-            className="rounded-xl"
-            style={{ marginBottom: 20 }}
-          />
+          <TextFiled id="input" name="input" placeholder="write in input" className="rounded-xl" style={{ marginBottom: 20 }} />
 
           <Block tag="p">This is a simple test using Basic components with enhanced styling.</Block>
 
-          <TextFiled
-            id="input2"
-            name="input2"
-            placeholder="write in input2"
-            className="rounded-3xl"
-            style={{ marginBlock: '20px 30px' }}
-          />
+          <TextFiled id="input2" name="input2" placeholder="write in input2" className="rounded-3xl" style={{ marginBlock: "20px 30px" }} />
 
           <Block>
             <Link href="/" className="button-primary mx-auto">
