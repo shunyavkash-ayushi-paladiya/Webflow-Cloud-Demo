fc5785adb1bac29e6b1861a170a6e7dcd476051c
Ayushi Paladiya <ayushi.paladiya@shunyavkash.com>
2025-11-21 16:50:46 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next';
 
-const basePath = '/webflow-cloud-demo';
+// const basePath = '/webflow-cloud-demo';
 
 const nextConfig: NextConfig = {
-  basePath,
+  // basePath,
   publicRuntimeConfig: {
-    basePath,
+    // basePath,
   },
 };
 
